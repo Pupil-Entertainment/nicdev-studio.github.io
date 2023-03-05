bf8ccf897c8fd8e9f348ebc1978ab0d245132e00
Nicholas Christian <66623456+NicDev-Studio@users.noreply.github.com>
2023-03-05 18:33:53 +0700

    update LatestYoutubeVideo.js

--- LatestYoutubeVideo.js
+++ LatestYoutubeVideo.js
@@ -1,26 +1,3 @@
-gapi.load('client', function() {
-   gapi.client.init({
-      apiKey: 'AIzaSyBUSDnWWT4LE-idCJjdlyRVUAxZOPKpCSI'
-   }).then(function() {
-      return gapi.client.request({
-         'path': 'https://www.googleapis.com/youtube/v3/search',
-         'params': {
-            'part': 'snippet',
-            'channelId': '',
-            'order': 'date',
-            'maxResults': 1
-         }
-      });
-   }).then(function(response) {
-      var videoId = response.result.items[0].id.videoId;
-      console.log(videoId);
-      var iframe = document.querySelector('#latest-video');
-      iframe.setAttribute('src', 'https://www.youtube.com/embed/' + videoId);
-   }, function(reason) {
-      console.error('Error: ' + reason.result.error.message);
-   });
-});
-
 $(function() {  
    var API_KEY = 'AIzaSyBUSDnWWT4LE-idCJjdlyRVUAxZOPKpCSI';
    var CHANNEL_ID = 'UCnR8Z34MIYgugRLunp9hJDQ';
