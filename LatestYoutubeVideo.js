10dce80d09ed54d19939515cc284d8133a09cd4d
Nicholas Christian <66623456+NicDev-Studio@users.noreply.github.com>
2023-03-05 18:33:25 +0700

    update LatestYoutubeVideo.js

--- LatestYoutubeVideo.js
+++ LatestYoutubeVideo.js
@@ -6,7 +6,7 @@ gapi.load('client', function() {
          'path': 'https://www.googleapis.com/youtube/v3/search',
          'params': {
             'part': 'snippet',
-            'channelId': 'UCnR8Z34MIYgugRLunp9hJDQ',
+            'channelId': '',
             'order': 'date',
             'maxResults': 1
          }
@@ -19,4 +19,23 @@ gapi.load('client', function() {
    }, function(reason) {
       console.error('Error: ' + reason.result.error.message);
    });
+});
+
+$(function() {  
+   var API_KEY = 'AIzaSyBUSDnWWT4LE-idCJjdlyRVUAxZOPKpCSI';
+   var CHANNEL_ID = 'UCnR8Z34MIYgugRLunp9hJDQ';
+
+   var GOOGLE_API_URL = 'https://www.googleapis.com/youtube/v3/channelItems?part=id%2C+snippet%2C+contentDetails&channelId='+CHANNEL_ID+ '&key=' + API_KEY + '&callback=showVideos';
+
+   $.ajax({
+    url: GOOGLE_API_URL,
+    dataType: 'jsonp',
+    crossDomain: true
+   });
+
+   window.showVideos = function(data) {
+    if (data.items && data.items.length > 0) {
+       $("#latest-video").html('<iframe width="640" height="360" src="http://www.youtube.com/embed/'+data.items[0].contentDetails.videoId+'" frameborder="0" allowfullscreen></iframe>');
+    }
+   }
 });
